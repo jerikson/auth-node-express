199d7ce4545049a972d78bfb12ebc49093364174
jerikson <jimmyerikson@gmail.com>
2019-04-24 13:58:40 +0200

    added dependency joi and created a basic schema, helpers/routeHelpers.

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,14 @@
 module.exports = {
+    // Email & password
+    // req.value.body
     signUp: async (req, res, next) => {
         console.log('UsersController.signUp() called!');
     },
+    // Generate token
     signIn: async (req, res, next) => {
         console.log('UsersController.signIn() called!');
     },
+    // 
     secret: async (req, res, next) => {
         console.log('UsersController.secret() called!');
     }
