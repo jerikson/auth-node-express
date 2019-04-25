afaffb5132af0991791e6603199969cb94379a30
jerikson <jimmyerikson@gmail.com>
2019-04-25 19:27:49 +0200

    Added dependency mongoose and created handle for it in the app.js.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 var app = express();
 
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/users', require('./routes/users'));
+
 
 // Start the server
 const port = process.env.PORT || 3000;
