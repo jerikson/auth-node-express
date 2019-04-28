b7d3e8c323e15746140cb1953ddf1435cb1e1204
jerikson <jimmyerikson@gmail.com>
2019-04-28 20:10:54 +0200

    Added code for checking if a user already exists, by email.

--- controllers/users.js
+++ controllers/users.js
@@ -4,22 +4,29 @@ module.exports = {
     // Email & password
     // req.value.body
     signUp: async (req, res, next) => {
-        console.log('UsersController.signUp() called!');
-        
         const { email, password } = req.value.body;
+        
+        // Check user already exists, by email
+        const foundUser = await User.findOne({ email });
+        if (foundUser) { 
+            return res.status(403).send({ error: 'Email is already in use'})
+        }
+
+        // Create a new user
         const newUser = new User({ email, password });
         await newUser.save();
         
+        // Respond with token
         res.json({user: 'created'});
     }, 
 
     // Generate token
     signIn: async (req, res, next) => {
-        console.log('UsersController.signIn() called!');
+    
     },
 
     // 
     secret: async (req, res, next) => {
-        console.log('UsersController.secret() called!');
+      
     }
 }
