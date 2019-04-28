29e2547f09e13cf4d2f16be188fcd0cf7affa0f1
jerikson <jimmyerikson@gmail.com>
2019-04-28 20:47:11 +0200

    Updated the models/users.js schema.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Auth');
 
 var app = express();
@@ -13,7 +14,6 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/users', require('./routes/users'));
-
 
 // Start the server
 const port = process.env.PORT || 3000;
