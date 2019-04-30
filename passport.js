2fa4d8b80920d60c64f15886b42e90be0be34f9f
jerikson <jimmyerikson@gmail.com>
2019-04-30 18:35:17 +0200

    Updated passport-jwt handle.

--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
-const { ExtractJwt } = require('passport-jwt');
+const ExtractJwt = require('passport-jwt');
 const { JWT_SECRET } = require('./configuration');
 const User = require('./models/user');
 
