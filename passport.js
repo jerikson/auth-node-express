abe309f24158317e5c2d0fbeddca362869b631ec
jerikson <jimmyerikson@gmail.com>
2019-05-02 22:00:35 +0200

    Fixed typo of resource: bcrypt import..

--- passport.js
+++ passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
-const LocalStrategy = requre('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const { JWT_SECRET } = require('./configuration');
 const User = require('./models/user');
 
