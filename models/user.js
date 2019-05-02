d71effc4a0e032314fa2f56018ebe744e60c2a0b
jerikson <jimmyerikson@gmail.com>
2019-05-02 21:56:38 +0200

    Added a pre-function to generate and assign a hash + salt to users password.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 // Create a Schema
@@ -13,6 +14,24 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+
+
+// Pre middleware functions are executed one after another, when each middleware calls next.
+userSchema.pre('save', async function(next) {
+    try {
+        // Generate a salt (10 rounds)
+        const salt = await bcrypt.genSalt(10);
+        // Generate a password hash (salt + hash)
+        const passwordHash = await bcrypt.hash(this.password, salt);
+        // Re-assign hashed version over origingal plain text password
+        this.password = passwordHash;
+        next();
+    } catch(error) {
+        next(error);
+    }
+
 });
 
 // Create a model
